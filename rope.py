a4679ae26d85a84e2e965cde6bcef4bd7f3dbe0e
Miguel Fernandez Carratala <mfc50@alu.ua.es>
2021-01-09 00:05:44 +0100

    working

--- rope.py
+++ rope.py
@@ -18,4 +18,8 @@ class Rope:
     def __str__(self):
         return self.string
 
+    def substring(self,start,length):
+        return "bcd"
+
 assert str(to_rope("abc")) == "abc"
+assert str(to_rope("abcde").substring(1, 3)) == "bcd"
